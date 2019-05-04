a5b8951e91f3aeb71e1fa74223c9991cb968a7ef
Tobias Hagemann <32563473+TBHagemann@users.noreply.github.com>
2019-05-04 18:56:32 +0200

    Oprettet get metoder til recipes som en service

---
+++ FoodService/IRecipeService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace FoodService
+{
+	[ServiceContract]
+	public interface IRecipeService
+	{
+        [OperationContract]
+        Recipe getRecipes(int id);
+
+	}
+    
+}
