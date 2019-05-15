0dffa92e0e73cfc747da1d62873591ce91158199
Zeydel <mads_seydel@yahoo.dk>
2019-05-15 12:34:32 +0200

    Skud ud til ViewModels

---
+++ FoodService/ViewModels/ViewFoodTrivia.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodService.ViewModels
+{
+	class ViewFoodTrivia
+	{
+		public ViewFoodTrivia(FoodTrivia foodTrivia)
+		{
+			Id = foodTrivia.Id;
+			Food_trivia = foodTrivia.Food_trivia;
+		}
+
+		public int Id { get; set; }
+		public string Food_trivia { get; set; }
+
+	}
+}
